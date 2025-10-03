caa3fbb03597252712d9c8094496d114864ad595
GabDev <gabmldev@proton.me>
2025-10-03 13:01:00 -0500

    core(init-project): add new dependencies and other stuff
    Signed-off-by: GabDev <gabmldev@proton.me>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import pdf from "astro-pdf";
+
 export default defineConfig({
   root: "./",
   base: "/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [pdf()],
 });
